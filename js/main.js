cb95850c1c2f116d9eaf46018bf80829a4ff5ee7
Abdullah Velisoy <abdullahvelisoy@gmail.com>
2023-07-24 21:31:52 +0400

    Add javascript codes

---
+++ js/main.js
@@ -0,0 +1,106 @@
+fetch("../data/projects.json")
+  .then((response) => response.json())
+  .then((json) => {
+    document.querySelector("#project_count").innerHTML =
+      json.projects.length + "+";
+    let projectsDiv = document.querySelector(
+      "#resume > div > div > div.resume-timeline"
+    );
+    for (let p of json.projects) {
+      projectsDiv.insertAdjacentHTML(
+        "beforeend",
+        `<div class="item scroll-animation" data-animation="fade_from_right">
+        <span class="date">${p.startDate + " ~ " + p.endDate}</span>
+        <h2>${p.name}</h2>
+        <p>${p.description}</p>
+    </div>`
+      );
+    }
+  });
+
+fetch("../data/teams.json")
+  .then((response) => response.json())
+  .then((json) => {
+    let teamsDiv = document.querySelector(
+      "#skills > div > div > div.row.skills.text-center"
+    );
+    let teamsDiv2 = document.querySelector("#pricing > div > div > div > div");
+    for (let t of json.teams) {
+      teamsDiv.insertAdjacentHTML(
+        "beforeend",
+        `<div class="col-md-3 scroll-animation" data-animation="fade_from_left">
+        <div class="skill">
+            <div class="skill-inner">
+                <img src="assets/images/${t.imageURL}" style="background-size: cover;">
+                <h1 class="percent">${t.name}</h1>
+            </div>
+            <p class="name">${t.competitions[0]}</p>
+        </div>
+    </div>`
+      );
+      let teamMembersHTML = "";
+      for (const m of t.members) {
+        teamMembersHTML += `<li>${m}</li>`;
+      }
+      teamsDiv2.insertAdjacentHTML(
+        "beforeend",
+        `
+      <div class="col-md-6 scroll-animation" data-animation="fade_from_left">
+                      <div class="pricing-table">
+                        <div class="pricing-table-header">
+                          <div class="top d-flex justify-content-between align-items-start">
+                            <h4>${t.date}</h4>
+                            <p class="text-right">
+                            ${t.competitions[0]}
+                            </p>
+                          </div>
+                          <h1>${t.name}</h1>
+                        </div>
+                        <ul class="feature-lists">
+                            ${teamMembersHTML}
+                        </ul>
+                      </div>
+                    </div>
+      `
+      );
+    }
+  });
+
+fetch("../data/events.json")
+  .then((response) => response.json())
+  .then((json) => {
+    let eventsDiv = document.querySelector(
+      "#portfolio > div > div > div.row.portfolio-items"
+    );
+    for (let e of json.events) {
+      let tagsHTML = "";
+      for (const t of e.tags) {
+        tagsHTML += `<li>
+        <span>${t}</span>
+        </li>`;
+      }
+      eventsDiv.insertAdjacentHTML(
+        "beforeend",
+        `
+        <div class="col-md-${
+          e.type === "medium" ? "12" : "6"
+        } scroll-animation" data-animation="fade_from_bottom">
+        <div class="portfolio-item portfolio-half${
+          e.type === "medium" ? "full" : "half"
+        }">
+            <div class="portfolio-item-inner">
+            <a href="assets/images/portfolio3.jpg" data-lightbox="example-1">
+                <img src="assets/images/${e.imageURL}" alt="Portfolio">
+            </a>
+
+            <ul class="portfolio-categories">
+                ${tagsHTML}
+            </ul>
+            </div>
+            <h2><a href="#">${e.name}</a></h2>
+        </div>
+        </div>
+        `
+      );
+    }
+  });
